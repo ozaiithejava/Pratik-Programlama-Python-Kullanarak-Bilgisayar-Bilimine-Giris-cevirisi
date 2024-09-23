4f7c96e69110550dbdd071deb9d1db4b166a5c5b
yiğit <82784013+ozaiithejava@users.noreply.github.com>
2024-09-24 01:00:56 +0300

    Create addition.py

---
+++ examples/opertors/shortcuts/addition.py
@@ -0,0 +1,11 @@
+# Toplama Kısa Yolu
+
+# Değişkenlerimiz
+x = 10
+y = 5
+
+# Toplamanın kısa yolu bu şekilde 
+x += 5  # klasik yol : x = x + 5
+
+print("x:", x)  # Çıktı: x: 15
+print("y:", y)  # Çıktı: y: 5
